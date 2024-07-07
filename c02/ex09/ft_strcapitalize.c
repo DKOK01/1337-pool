2b16bbeb185c5def8899e2c058667d247a483653
DKOK01 <aymansadik2005@gmail.com>
2024-07-07 08:58:01 +0100

    gg
    gg

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aysadeq <aysadeq@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 20:11:48 by aysadeq           #+#    #+#             */
+/*   Updated: 2024/07/03 21:53:10 by aysadeq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + ('a' - 'A');
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	new_word;
+
+	i = 0;
+	new_word = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (new_word)
+			{
+				str[i] = str[i] - 32;
+			}
+			new_word = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			new_word = 0;
+		else
+			new_word = 1;
+		i++;
+	}
+	return (str);
+}
