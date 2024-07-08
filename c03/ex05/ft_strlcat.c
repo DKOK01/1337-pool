0c28d22f46c539d0229e0890169ae9112e0376a2
DKOK01 <aymansadik2005@gmail.com>
2024-07-08 10:48:04 +0100

    gg
    gg

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aysadeq <aysadeq@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 14:37:11 by aysadeq           #+#    #+#             */
+/*   Updated: 2024/07/07 15:09:36 by aysadeq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	src_len = 0;
+	i = 0;
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	if (size <= dest_len)
+		return (size + src_len);
+	while (src[i] != '\0' && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
